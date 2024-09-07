de90b08c84a18b2cff7cceb2c98c024c546425d4
Ruben Merson Nzaou <rm.nzaou.etudes@gmail.com>
2024-09-07 18:53:28 +0200

    feat: added auth middleware

--- final_project/index.js
+++ final_project/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const session = require("express-session");
-const customer_routes = require("./router/auth_users.js").authenticated;
-const genl_routes = require("./router/general.js").general;
+const customerRoutes = require("./router/auth-users.js").authenticated;
+const generalRoutes = require("./router/general.js").general;
+const axiosRoutes = require("./router/axios-routes.js").axiosRouter;
 
 const app = express();
+const PORT = 3000;
 
 app.use(express.json());
 
@@ -19,11 +21,34 @@ app.use(
 
 app.use("/customer/auth/*", function auth(req, res, next) {
   //Write the authenication mechanism here
+  try {
+    const { accessToken, username } = req.session.authorization;
+
+    // check token integrity
+    if (!accessToken) {
+      throw new Error("Unauthorized");
+    }
+
+    const { password } = jwt.verify(accessToken, "access");
+
+    req.user = {
+      username,
+      password,
+    };
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(403).json({ message: "User not authenticated" });
+  }
 });
 
-const PORT = 5000;
+app.use("/customer", customerRoutes);
+app.use("/", generalRoutes);
+app.use("/axios", axiosRoutes);
 
-app.use("/customer", customer_routes);
-app.use("/", genl_routes);
+app.use((_req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
 app.listen(PORT, () => console.log("Server is running"));
